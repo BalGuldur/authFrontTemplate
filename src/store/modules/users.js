d18637b081236d670af62a1f9a049d845af5d9ce
Aleksandr Krulov <a.krulov@gmail.com>
2017-11-15 18:27:21 +0300

    add users store

---
+++ src/store/modules/users.js
@@ -0,0 +1,22 @@
+const state = {
+  users: {}
+}
+
+const getters = {
+  users: state => state.users
+}
+
+const actions = {
+  fetchUsers () {
+  }
+}
+
+const mutations = {
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
